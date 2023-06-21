eb848ae23ba1f7e4524b413e36f5aded5870e579
Muhammad Ashraf <121554755+MuhammadAshraf23@users.noreply.github.com>
2023-06-21 13:01:20 +0500

    update olx

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function loginOpen(){
+    document.getElementById("login").classList.remove("hidden");
+    window.location.href="./login.html"
+  }
+
+ function phoneLogin(){
+    document.getElementById('phonelog').innerHTML
+    window.location.href="./signin.html"
+    localStorage.setItem("email-input")
+    localStorage.setItem('password-input')
+}
+  function login(){
+    
+  }
+      
+
+  
+  
+
+
+
+
+
+
+
+  
